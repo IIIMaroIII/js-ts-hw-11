6f2e770be0bddb5ff6c306b6bfbf5d3057329b5c
rod <matveev.r.v@icloud.com>
2025-11-19 23:51:51 +0200

    quick_commit

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,53 @@
-import './css/reset.css';
-import './css/style.css';
+import "./css/reset.css";
+import "./css/style.css";
 
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-import PixabayApi from './ts/PixabayApi';
-import RenderFactory from './ts/RenderFactory';
-import { getRef } from './utils/getRef';
+import PixabayApi from "./ts/PixabayApi";
+import RenderFactory from "./ts/RenderFactory";
+import { getRef } from "./utils/getRef";
 
 const renderFactory = new RenderFactory();
 const pixabayApi = new PixabayApi();
 
 export interface Refs {
-	form: HTMLFormElement | null;
+  form: HTMLFormElement | null;
 }
 
 const refs: Refs = {
-	form: getRef<HTMLFormElement>('[data-form]'),
+  form: getRef<HTMLFormElement>("[data-form]"),
 };
 
-refs.form!.addEventListener('submit', onSearch);
+refs.form!.addEventListener("submit", onSearch);
 
 function onSearch(e: Event): void {
-	e.preventDefault();
-	renderFactory.clear();
+  e.preventDefault();
+  renderFactory.clear();
 
-	const target = e.target as HTMLFormElement;
-	const formData: FormData = new FormData(target);
+  const target = e.target as HTMLFormElement;
+  const formData: FormData = new FormData(target);
 
-	const SEARCH_TEXT = 'search-text';
-	const q = formData.get(SEARCH_TEXT);
+  const SEARCH_TEXT = "search-text";
+  const q = formData.get(SEARCH_TEXT);
 
-	if (typeof q !== 'string') return;
+  if (typeof q !== "string") return;
 
-	pixabayApi
-		.setQuery(q)
-		?.fetchImages()
-		.then(data => {
-			if (data.hits.length === 0) {
-				throw new Error('Sorry, there are no images matching your search query. Please try again!');
-			}
-			renderFactory.setImages(data.hits).createMarkup().renderMarkup();
-			pixabayApi.increasePage();
-		})
-		.catch(err =>
-			iziToast.error({
-				message: err.message,
-			}),
-		);
+  pixabayApi
+    .setQuery(q)
+    ?.fetchImages()
+    .then((data) => {
+      if (data.hits.length === 0) {
+        throw new Error(
+          "Sorry, there are no images matching your search query. Please try again!"
+        );
+      }
+      renderFactory.setImages(data.hits).createMarkup().renderMarkup();
+      pixabayApi.increasePage();
+    })
+    .catch((err) =>
+      iziToast.error({
+        message: err.message,
+      })
+    );
 }
